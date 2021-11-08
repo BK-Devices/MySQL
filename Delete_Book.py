4ad9ae7326c8b838e41784b2af2b8bed39111386
Bhavesh Kale <91716644+BK-Devices@users.noreply.github.com>
2021-11-08 12:11:06 +0530

    Update Delete_Book.py

--- Delete_Book.py
+++ Delete_Book.py
@@ -24,7 +24,7 @@ try:
         print()
         ch = input('Do you want remove Book from database - Yes/No : ')
         if ch.upper() == 'YES':
-            curs.execute("delete from books where Book_Id = %d" %id)
+            curs.execute("delete from Books where Book_Id = %d" %id)
             con.commit()
             
             print()
